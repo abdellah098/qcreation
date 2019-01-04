e2c7b689a980e9f5bbfa72bb5eb3b2898d14ae60
yacabdel <saley.abdellah@gmail.com>
2019-01-04 17:05:38 +0100

    insertion de bouton,de font,de curseur

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc,argv);
     QPushButton bouton("Hello world");
+    bouton.setText("Hello the world");
+    bouton.setToolTip("Texte d'aide");
+    bouton.setFont(QFont("aakar",15,QFont::Bold,true));
+    bouton.setCursor(Qt::PointingHandCursor);
     bouton.show();
 
     return app.exec();
