4ceebd4e3d07cdd868d9d0bb3f7feca621b3f77c
yacabdel <saley.abdellah@gmail.com>
2019-01-04 22:18:05 +0100

    creation d'une fenetre contenent le bouton

--- test/main.cpp
+++ test/main.cpp
@@ -4,12 +4,19 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc,argv);
-    QPushButton bouton("Hello world");
-    bouton.setText("Hello the world");
+    //QPushButton bouton("Hello world");
+    //bouton.setText("Hello the world");
+    //creation d'une fenetre qui contiendra le bouton
+
+    QWidget fenetre;
+    fenetre.setFixedSize(300,200);
+   QPushButton bouton("salut les potes",&fenetre);
     bouton.setToolTip("Texte d'aide");
     bouton.setFont(QFont("aakar",15,QFont::Bold,true));
     bouton.setCursor(Qt::PointingHandCursor);
-    bouton.show();
+    bouton.setIcon(QIcon("pic.png"));//ne marche pas
+   // bouton.show();
+    fenetre.show();
 
     return app.exec();
 }
